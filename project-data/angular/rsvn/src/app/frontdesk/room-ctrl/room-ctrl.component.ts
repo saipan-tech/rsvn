12861f9de9096eb571ed6d36d5f35c44cdb69cad
JC Clark <jc@saipantech.com>
2021-10-10 21:25:09 -0700

    last css

--- project-data/angular/rsvn/src/app/frontdesk/room-ctrl/room-ctrl.component.ts
+++ project-data/angular/rsvn/src/app/frontdesk/room-ctrl/room-ctrl.component.ts
@@ -2,12 +2,14 @@ import { Component, Input, Output, OnChanges, OnInit, SimpleChange, SimpleChange
 import { IBldg } from '@app/_interface/bldg';
 import { IRsvn } from '@app/_interface/rsvn'
 import { IRoom } from '@app/_interface/room'
+import { IRate } from '@app/_interface/rate'
 import { IRoominfo } from '@app/_interface/roominfo'
 import { IGuest } from '@app/_interface/guest'
 import { GenericService } from '@app/_services/generic.service';
 import { RsvnService } from '@app/_services/rsvn.service';
 import { RoomService } from '@app/_services/room.service';
 import {MatRadioModule} from '@angular/material/radio';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-room-ctrl',
@@ -28,6 +30,7 @@ export class RoomCtrlComponent implements OnInit, OnChanges {
 
   bldgList: IBldg[] = []
   roomList: IRoominfo[] = []
+  rateList: IRate[] = []
 
   currRoomList : any[] = []
   
@@ -74,8 +77,8 @@ export class RoomCtrlComponent implements OnInit, OnChanges {
       })
   }
 
-  makeList() {
-    this.dispList = []
+  splitRate() {
+    let rateSet = new Set()
     this.bldgList.forEach(
       bld => {
         let rooms = this.availRoominfo.filter(r => r.bldg == bld.id)
@@ -83,19 +86,38 @@ export class RoomCtrlComponent implements OnInit, OnChanges {
         this.dispList.push({ bldg, rooms })
       }
     )
+
+  }
+
+  makeList() {
+    this.dispList = []
+    
+    this.bldgList.forEach(
+      bld => {
+        let rooms = this.availRoominfo.filter(r => r.bldg == bld.id)
+        let bldg = bld.name
+        this.dispList.push({ bldg, rooms })
+      }
+    )
+  
   }
 
 
   bldgText(bldg:number) {
     return this.bldgList.find(b => b.id==bldg)?.name
-
   }
+
   ngOnChanges(changes: SimpleChanges) {
     this.ngOnInit()
   }
 
   ngOnInit(): void {
     if (this.currRsvn && this.currRsvn.id) {
+      this.genericService.getItemList("rate")
+        .subscribe(
+          data => {
+            this.rateList = data
+          })
     this.roomService.getRsvnRoom(this.currRsvn.id).subscribe(
       rooms => {
         this.currNumRooms = rooms.length
@@ -104,6 +126,7 @@ export class RoomCtrlComponent implements OnInit, OnChanges {
     )
     }
     if (this.currRsvn && this.currRsvn.dateIn && this.currRsvn.dateOut) {
+      // we are creating our UnAssigned Rooms here
       this.roomService.availableRooms(this.currRsvn.dateIn,this.currRsvn.dateOut)
         .subscribe(avail => {
           this.availRoominfo = avail
@@ -113,9 +136,9 @@ export class RoomCtrlComponent implements OnInit, OnChanges {
               this.makeList()
             })
         })
-      this.roomService.unavailableRooms(this.currRsvn.dateIn,this.currRsvn.dateOut)
+        // we are creating our Assigned Rooms here
+        this.roomService.unavailableRooms(this.currRsvn.dateIn,this.currRsvn.dateOut)
         .subscribe(unavail => {
-
           this.unavailRoominfo = unavail 
           this.roomService.getRsvnRoom(this.currRsvn.id)
           .subscribe(rroom => {
