4c6f3677883b5cb776a22636cc50fad5d0e8285c
Franklin Clark <franklin@modeanalytics.com>
2021-10-20 00:27:07 -0700

    rsvn-12: Generate and import danger dialog

--- project-data/angular/rsvn/src/app/shared/shared.module.ts
+++ project-data/angular/rsvn/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DangerDialogComponent } from './danger-dialog/danger-dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DangerDialogComponent
+  ],
   imports: [
     CommonModule
   ]
