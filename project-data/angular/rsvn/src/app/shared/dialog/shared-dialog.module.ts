d8a055140aab8642928e5a3b3ce5d07fec34d097
Franklin Clark <franklin@modeanalytics.com>
2021-10-20 02:35:44 -0700

    rsvn-12: Add icon to title

--- project-data/angular/rsvn/src/app/shared/dialog/shared-dialog.module.ts
+++ project-data/angular/rsvn/src/app/shared/dialog/shared-dialog.module.ts
@@ -4,6 +4,7 @@ import { ManagerService } from './manager/manager.service';
 import { MatDialogModule } from "@angular/material/dialog";
 import {DangerDialogComponent} from "@app/shared/dialog/danger-dialog/danger-dialog.component";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {MatButtonModule} from "@angular/material/button";
     CommonModule,
     MatButtonModule,
     MatDialogModule,
+    MatIconModule,
   ],
   providers: [
     ManagerService,
