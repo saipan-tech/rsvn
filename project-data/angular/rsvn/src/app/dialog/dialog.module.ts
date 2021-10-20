ecb877c92d305df53b69281fd2ed673d5cc21fe5
Franklin Clark <franklin@modeanalytics.com>
2021-10-20 00:44:22 -0700

    rsvn-12: Move dialog service to dialog module

--- project-data/angular/rsvn/src/app/dialog/dialog.module.ts
+++ project-data/angular/rsvn/src/app/dialog/dialog.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogService } from './dialog.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    DialogService,
   ]
 })
 export class DialogModule { }
