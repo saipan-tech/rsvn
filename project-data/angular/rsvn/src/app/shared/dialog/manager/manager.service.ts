2ca46ce2c011a299c1ae39f7e34b124451272262
Franklin Clark <franklin@modeanalytics.com>
2021-10-20 01:23:15 -0700

    rsvn-12: Add open method

--- project-data/angular/rsvn/src/app/shared/dialog/manager/manager.service.ts
+++ project-data/angular/rsvn/src/app/shared/dialog/manager/manager.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable, TemplateRef} from '@angular/core';
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {ComponentType} from "@angular/cdk/overlay";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+
+  openDialog<T>(dialogComponent: ComponentType<T> | TemplateRef<T>, config: MatDialogConfig = {}) {
+    return this.dialog.open(dialogComponent, config);
+  }
 }
