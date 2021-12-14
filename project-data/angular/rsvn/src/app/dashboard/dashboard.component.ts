392b3521a55c59422e06d15fcf7c58318e664ebe
Franklin Clark <franklin@modeanalytics.com>
2021-12-14 02:43:04 -0800

    rsvn-17: Display arrivals container

--- project-data/angular/rsvn/src/app/dashboard/dashboard.component.ts
+++ project-data/angular/rsvn/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {ThemePalette} from '@angular/material/core';
  @Component({
@@ -7,6 +7,9 @@ import {ThemePalette} from '@angular/material/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  @ViewChild('arrivals', {static: false})
+  arrivals: any;
 
   constructor(
     private router: Router,
