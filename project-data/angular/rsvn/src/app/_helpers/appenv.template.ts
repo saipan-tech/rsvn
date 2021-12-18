ef906339d0e6916cad5baf77fadeaf90741db8ed
Franklin Clark <franklin@modeanalytics.com>
2021-12-17 21:32:02 -0800

    Fix appenv template

--- project-data/angular/rsvn/src/app/_helpers/appenv.template.ts
+++ project-data/angular/rsvn/src/app/_helpers/appenv.template.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 Injectable()
 export class AppEnv {
   public API_URL = 'http://1.1.1.1:9000';
-  public WEB_URL = 'http://1.1.1.1:9000';
+  public WEB_API = 'http://1.1.1.1:9000';
 }
