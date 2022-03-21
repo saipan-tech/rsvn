2546795cc954638715c7895b5acebdda2dbf4587
Franklin Clark <franklin@modeanalytics.com>
2022-03-21 02:56:49 -0700

    Add pdfmake

--- project-data/angular/rsvn/src/app/charges/charge-ctrl/charge-ctrl.component.ts
+++ project-data/angular/rsvn/src/app/charges/charge-ctrl/charge-ctrl.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, Output, OnChanges, OnInit, SimpleChange, SimpleChanges, EventEmitter } from '@angular/core';
 import { IRsvn } from '@app/_interface/rsvn';
 import { ICharge } from '@app/_interface/charge';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-charge-ctrl',
@@ -48,7 +52,30 @@ export class ChargeCtrlComponent implements OnInit {
     
   }
 
-  handlePrintInvoiceClick() {
+  handlePrintInvoiceClick() { 
+    var docDefinition = {
+      content: [
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [ '*', 'auto', 100, '*' ],
+    
+            body: [
+              [ 'First', 'Second', 'Third', 'The last one' ],
+              [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+              [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+            ]
+          }
+        }
+      ]
+    };
+    
+    // download the PDF
+    const pdf = pdfMake.createPdf(docDefinition)
+    pdf.download();
   }
 
   //--------------------------
