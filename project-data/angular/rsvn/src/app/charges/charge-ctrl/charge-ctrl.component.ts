b938040d8203726712d1e59551bf813fe6a1e890
Franklin Clark <franklin@modeanalytics.com>
2022-05-07 23:29:28 -0700

    Generate PDF when print invoice clicked

--- project-data/angular/rsvn/src/app/charges/charge-ctrl/charge-ctrl.component.ts
+++ project-data/angular/rsvn/src/app/charges/charge-ctrl/charge-ctrl.component.ts
@@ -3,15 +3,7 @@ import { DialogManagerService } from '@app/shared/dialog';
 import { ICharge } from '@app/_interface/charge';
 import { IPayment } from "@app/_interface/payment";
 import { IRsvn } from '@app/_interface/rsvn';
-import { ChargeService } from '@app/_services/charge.service';
-import { RoomService } from '@app/_services/room.service';
-import { format, parseISO } from 'date-fns';
-import * as pdfMake from "pdfmake/build/pdfmake";
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-import { Alignment } from "pdfmake/interfaces";
 import { ChargeInvoiceComponent } from '../charge-invoice/charge-invoice.component';
-
-(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-charge-ctrl',
@@ -21,8 +13,6 @@ import { ChargeInvoiceComponent } from '../charge-invoice/charge-invoice.compone
 export class ChargeCtrlComponent implements OnInit {
 
   constructor(
-    private roomService: RoomService,
-    private chargeService: ChargeService,
     private dialogManagerService: DialogManagerService
   ) { }
 
@@ -34,10 +24,6 @@ export class ChargeCtrlComponent implements OnInit {
   pmtSubTotal = 0
   chgSubTotal = 0
   roomSubTotal = 0
-  fullRoomList: any[] = [];
-  roomChargeData: any = []
-  chargeList: ICharge[] = [];
-  paymentList: IPayment[] = [];
 
   changeCharge(event:ICharge) {
       this.currCharge = event
@@ -64,149 +50,19 @@ export class ChargeCtrlComponent implements OnInit {
   }
 
   handleInvoiceClick(): void {
-    this.dialogManagerService.openDialog<ChargeInvoiceComponent>(ChargeInvoiceComponent);
+    this.dialogManagerService.openDialog<ChargeInvoiceComponent>(ChargeInvoiceComponent, {
+      data: {
+        pmtSubTotal: this.pmtSubTotal,
+        roomSubTotal: this.roomSubTotal,
+        chgSubTotal: this.chgSubTotal,
+        grandTotal: this.grandTotal,
+        currRsvnId: this.currRsvn.id
+      }
+    });
   }
 
-  handlePrintInvoiceClick() {
-    var docDefinition = {
-      content: [
-        {text: 'Invoice', style: 'header'},
-        {text: 'Room Charges', style: 'subheader'},
-        {
-          layout: 'lightHorizontalLines',
-          table: {
-            headerRows: 1,
-            widths: [ 70, '*', 50, 100 ],
-            body: [
-              [ 'Date', 'Room Name', 'Room #', 'Price' ],
-              ...this.roomCharges(),
-              ['', '', { text: 'Subtotal', style: 'totalCell'}, { text: `$${this.roomSubTotal.toFixed(2)}`, style: 'alignRight' }]
-            ]
-          },
-        },
-        {text: 'Charges', style: 'subheader'},
-        {
-          style: 'invoiceTable',
-          layout: 'lightHorizontalLines',
-          table: {
-            headerRows: 1,
-            widths: [ 70, '*', '*', 40, 50, 100 ],
-            body: [
-              [ 'Date', 'Item', 'Description', 'Count', 'Unit', 'Amount' ],
-              ...this.charges(),
-              ['', '', '', '', { text: 'Subtotal', style: 'totalCell' }, { text: `$${this.chgSubTotal.toFixed(2)}`, style: 'alignRight' }]
-            ]
-          }
-        },
-        {text: 'Payments', style: 'subheader'},
-        {
-          style: 'invoiceTable',
-          layout: 'lightHorizontalLines',
-          table: {
-            headerRows: 1,
-            widths: [ 70,  '*', '*', 100 ],
-            body: [
-              [ 'Date', 'Item', 'Description', 'Amount' ],
-              ...this.payments(),
-              ['',  '', { text: 'Subtotal', style: 'totalCell' }, { text: `$${this.pmtSubTotal.toFixed(2)}`, style: 'alignRight' }]
-            ]
-          }
-        },
-        {text: 'Total', style: 'subheader'},
-        {
-          style: 'invoiceTable',
-          layout: 'lightHorizontalLines',
-          table: {
-            headerRows: 1,
-            widths: [ '*', 50, 100 ],
-            body: [
-              [  '', '', '' ],
-              [ '', { text: 'Taxes', style: 'alignRight' }, { text: `$${0}`, style: 'alignRight' }],
-              [ '', { text: 'Other', style: 'alignRight' }, { text: `$${0}`, style: 'alignRight' }],
-              [ '', { text: 'Total', style: 'totalCell' }, { text: `$${this.grandTotal.toFixed(2)}`, style: 'alignRight' }]
-            ]
-          }
-        }
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          bold: true,
-          margin: [0, 0, 0, 10] as [number, number, number, number]
-        },
-        subheader: {
-          fontSize: 16,
-          bold: true,
-          margin: [0, 10, 0, 5] as [number, number, number, number]
-        },
-        invoiceTable: {
-          margin: [0, 5, 0, 15] as [number, number, number, number]
-        },
-        alignRight: {
-          alignment: 'right' as Alignment
-        },
-        totalCell: {
-          bold: true,
-          alignment: 'right' as Alignment
-        }
-      },
-    };
+  ngOnInit() {
 
-    // open new tab
-    var win = window.open('', '_blank');
-
-    // load pdf in new tab
-    pdfMake.createPdf(docDefinition).open({}, win);
-  }
-
-  roomCharges(): any[] {
-    return this.roomChargeData.reduce((accu: any[], roomCharges: any)=> {
-      roomCharges.charges.forEach((charge: any)=> {
-        accu.push([
-          format(parseISO(charge.date), 'MM/dd/yyyy'),
-          roomCharges.roominfo.rateAlias,
-          { text: roomCharges.roominfo.number, style: 'alignRight' },
-          { text: charge.amount, style: 'alignRight' }
-        ])
-      })
-
-      return accu;
-    }, []);
-  }
-
-  charges(): any[] {
-    return this.chargeList.map((charge)=> [
-      format(parseISO(charge.date), 'MM/dd/yyyy'),
-      charge.item,
-      charge.descr,
-      { text: charge.count, style: 'alignRight' },
-      { text: charge.unit, style: 'alignRight' },
-      { text: Number(charge.amount).toFixed(2), style: 'alignRight' },
-    ])
-  }
-
-  payments(): any[] {
-    return this.paymentList.map((payment: IPayment)=> [
-      format(parseISO(payment.date), 'MM/dd/yyyy'),
-      payment.item,
-      payment.descr,
-      { text: Number(payment.amount).toFixed(2), style: 'alignRight' },
-    ]);
-  }
-
-  //--------------------------
-  ngOnInit(): void {
-    this.chargeService.getRsvnRoomCharge(this.currRsvn.id).subscribe(data => {
-      this.roomChargeData = data;
-    })
-
-    this.chargeService.getRsvnCharge(this.currRsvn.id).subscribe(data => {
-      this.chargeList = data;
-    })
-
-    this.chargeService.getRsvnPayment(this.currRsvn.id).subscribe(data=> {
-      this.paymentList = data;
-    })
   }
 }
 
--- project-data/angular/rsvn/src/app/charges/charge-invoice/charge-invoice.component.ts
+++ project-data/angular/rsvn/src/app/charges/charge-invoice/charge-invoice.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChargeService } from '@app/_services/charge.service';
+import { RoomService } from '@app/_services/room.service';
+import { ICharge } from '@app/_interface/charge';
+import { IPayment } from "@app/_interface/payment";
+import { format, parseISO } from 'date-fns';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { Alignment } from "pdfmake/interfaces";
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-charge-invoice',
@@ -6,12 +17,163 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charge-invoice.component.scss']
 })
 export class ChargeInvoiceComponent implements OnInit {
+  fullRoomList: any[] = [];
+  roomChargeData: any = [];
+  chargeList: ICharge[] = [];
+  paymentList: IPayment[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      pmtSubTotal: number,
+      roomSubTotal: number,
+      chgSubTotal: number, 
+      grandTotal: number,
+      currRsvnId: number,
+    },
+    private roomService: RoomService,
+    private chargeService: ChargeService,
+  ){}
 
   handlePrintInvoiceClick(): void {
+    var docDefinition = {
+      content: [
+        {text: 'Invoice', style: 'header'},
+        {text: 'Room Charges', style: 'subheader'},
+        {
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ 70, '*', 50, 100 ],
+            body: [
+              [ 'Date', 'Room Name', 'Room #', 'Price' ],
+              ...this.roomCharges(),
+              ['', '', { text: 'Subtotal', style: 'totalCell'}, { text: `$${this.data.roomSubTotal.toFixed(2)}`, style: 'alignRight' }]
+            ]
+          },
+        },
+        {text: 'Charges', style: 'subheader'},
+        {
+          style: 'invoiceTable',
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ 70, '*', '*', 40, 50, 100 ],
+            body: [
+              [ 'Date', 'Item', 'Description', 'Count', 'Unit', 'Amount' ],
+              ...this.charges(),
+              ['', '', '', '', { text: 'Subtotal', style: 'totalCell' }, { text: `$${this.data.chgSubTotal.toFixed(2)}`, style: 'alignRight' }]
+            ]
+          }
+        },
+        {text: 'Payments', style: 'subheader'},
+        {
+          style: 'invoiceTable',
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ 70,  '*', '*', 100 ],
+            body: [
+              [ 'Date', 'Item', 'Description', 'Amount' ],
+              ...this.payments(),
+              ['',  '', { text: 'Subtotal', style: 'totalCell' }, { text: `$${this.data.pmtSubTotal.toFixed(2)}`, style: 'alignRight' }]
+            ]
+          }
+        },
+        {text: 'Total', style: 'subheader'},
+        {
+          style: 'invoiceTable',
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [ '*', 50, 100 ],
+            body: [
+              [  '', '', '' ],
+              [ '', { text: 'Taxes', style: 'alignRight' }, { text: `$${0}`, style: 'alignRight' }],
+              [ '', { text: 'Other', style: 'alignRight' }, { text: `$${0}`, style: 'alignRight' }],
+              [ '', { text: 'Total', style: 'totalCell' }, { text: `$${this.data.grandTotal.toFixed(2)}`, style: 'alignRight' }]
+            ]
+          }
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10] as [number, number, number, number]
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5] as [number, number, number, number]
+        },
+        invoiceTable: {
+          margin: [0, 5, 0, 15] as [number, number, number, number]
+        },
+        alignRight: {
+          alignment: 'right' as Alignment
+        },
+        totalCell: {
+          bold: true,
+          alignment: 'right' as Alignment
+        }
+      },
+    };
+
+    // open new tab
+    var win = window.open('', '_blank');
+
+    // load pdf in new tab
+    pdfMake.createPdf(docDefinition).open({}, win);
+  }
+
+  roomCharges(): any[] {
+    return this.roomChargeData.reduce((accu: any[], roomCharges: any)=> {
+      roomCharges.charges.forEach((charge: any)=> {
+        accu.push([
+          format(parseISO(charge.date), 'MM/dd/yyyy'),
+          roomCharges.roominfo.rateAlias,
+          { text: roomCharges.roominfo.number, style: 'alignRight' },
+          { text: charge.amount, style: 'alignRight' }
+        ])
+      })
+
+      return accu;
+    }, []);
+  }
+
+  charges(): any[] {
+    return this.chargeList.map((charge)=> [
+      format(parseISO(charge.date), 'MM/dd/yyyy'),
+      charge.item,
+      charge.descr,
+      { text: charge.count, style: 'alignRight' },
+      { text: charge.unit, style: 'alignRight' },
+      { text: Number(charge.amount).toFixed(2), style: 'alignRight' },
+    ])
+  }
+
+  payments(): any[] {
+    return this.paymentList.map((payment: IPayment)=> [
+      format(parseISO(payment.date), 'MM/dd/yyyy'),
+      payment.item,
+      payment.descr,
+      { text: Number(payment.amount).toFixed(2), style: 'alignRight' },
+    ]);
+  }
+
+  //--------------------------
+  ngOnInit(): void {
+    this.chargeService.getRsvnRoomCharge(this.data.currRsvnId).subscribe(data => {
+      this.roomChargeData = data;
+    })
+
+    this.chargeService.getRsvnCharge(this.data.currRsvnId).subscribe(data => {
+      this.chargeList = data;
+    })
+
+    this.chargeService.getRsvnPayment(this.data.currRsvnId).subscribe(data=> {
+      this.paymentList = data;
+    })
   }
 }
