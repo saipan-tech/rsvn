94f871a6642b0007069d07ebf90fea7fa4429c79
JC Clark <jc@saipantech.com>
2022-05-31 23:15:36 -0700

    service reservation looking better

--- project-data/angular/rsvn/src/app/admin/current-ctrl/svcrsvn/svcrsvn-list/svcrsvn-list.component.ts
+++ project-data/angular/rsvn/src/app/admin/current-ctrl/svcrsvn/svcrsvn-list/svcrsvn-list.component.ts
@@ -6,6 +6,11 @@ import { SvcrsvnEditComponent } from '@app/admin/current-ctrl/svcrsvn/svcrsvn-ed
 import { IRoominfo } from '@app/_interface/roominfo';
 import { GenericService } from '@app/_services/generic.service';
 import { Observable, of } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+import { RoominfoDataService } from '@app/_ngrxServices/roominfo-data.service';
+import { RoominfoEntityService } from '@app/_ngrxServices/roominfo-entity.service';
+import { BldgEntityService } from '@app/_ngrxServices/bldg-entity.service';
+import { RoomEntityService } from '@app/_ngrxServices/room-entity.service';
 
 
 @Component({
@@ -17,23 +22,27 @@ export class SvcrsvnListComponent implements OnInit {
 
   constructor(
     private dialog: MatDialog,
-    private genericService: GenericService
+    private genericService: GenericService,
+    private roominfoService: RoominfoEntityService,
+    private bldgService: BldgEntityService,
+    private roomService: RoomEntityService
 
   ) { }
 
-    currSvcRsvn:ISvcRsvn = {} as ISvcRsvn
-    svcList$:Observable<ISvcRsvn[]> = of([])    
+  currSvcRsvn: ISvcRsvn = {} as ISvcRsvn
+  svcList$: Observable<any[]> = of([])
+  warnSet$: Observable<any> = of()
 
-    @Input() currRoominfo:IRoominfo = {} as IRoominfo
-    
+  @Input() currRoominfo: IRoominfo = {} as IRoominfo
+
   //--------------------------
-  openDialog(currSvcRsvn:ISvcRsvn) {
+  openDialog(currSvcRsvn: ISvcRsvn) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.panelClass = [];
     dialogConfig.minWidth = '25%';
-    dialogConfig.width='90%';
+    dialogConfig.width = '90%';
     dialogConfig.data = {
       currSvcRsvn: currSvcRsvn
     }
@@ -46,13 +55,53 @@ export class SvcrsvnListComponent implements OnInit {
         }
       )
   }
-  editSvcrsvn(svc:ISvcRsvn) {
+  editSvcrsvn(svc: ISvcRsvn) {
     this.currSvcRsvn = svc
     this.openDialog(svc)
   }
+
   reload() {
-    this.svcList$ =  this.genericService.getItemList('svcrsvn')
+
+    this.warnSet$ = this.roomService.activeRoom$(this.currSvcRsvn.dateIn, this.currSvcRsvn.dateOut).pipe(
+      map(warn => {
+        let warnSet = new Set()
+        warn.forEach(w => warnSet.add(w.roominfo))
+        return warnSet
+      })
+    )
+    this.svcList$ =  this.genericService.getItemList('svcrsvn').pipe(
+        concatMap(svclist => this.roominfoService.entities$.pipe(
+          concatMap(roominfos => this.bldgService.entities$.pipe(
+            map(bldg => {
+              svclist.map(
+                sl => {
+                  sl.roomlist = []
+                  sl.roominfos.split(",").forEach(
+                    (ss: any) => {
+                      let ri: any = roominfos.find(ro => ro.id == ss)
+                      let bl: any = bldg.find(bb => bb.id == ri.bldg)
+                      sl.roomlist.push({
+                        id: ss,
+                        number: ri.number,
+                        bldg: bl.abbrev,
+                   
+                      })
+                    }
+                  )
+                  sl.roomlist.sort((a: any, b: any) => (a.bldg.localeCompare(b.bldg) || a.number.localeCompare(b.number)));
+                })
+              console.log(svclist)
+              return svclist
+            }
+        ))
+      ))
+    ))
   }
+
+
+
+
+
 
 
   ngOnInit(): void {
