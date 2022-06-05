5b27176f4d466595409e07b8e4c10c473ebe1e41
Franklin Clark <franklin@modeanalytics.com>
2022-06-04 22:24:15 -0700

    Revert "Run headless chrome for angular2 tests"
    This partially reverts commit b6d0f22bd1bc5fd0fdd435bc1be6844bbe8f0e05.
    
    # Conflicts:
    #	project-data/angular2/rsvnterm/src/app/_helpers/appenv/index.ts

--- project-data/angular/rsvn/custom-webpack.config.js
+++ project-data/angular/rsvn/custom-webpack.config.js
@@ -3,11 +3,8 @@ const webpack = require('webpack');
 module.exports = {
   resolve: {
     fallback: {
-      // fix for `dotenv` library
       fs: false,
-      // fix for `dotenv` library
       path: false,
-      // fix for `dotenv` library
       os: false,
     }
   }
--- project-data/angular2/rsvnterm/src/app/_helpers/appenv/index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-import { Injectable } from '@angular/core';
-
-Injectable()
-export class AppEnv {
-  public API_URL = process.env['API_URL'] || '';
-  public WEB_API = process.env['WEB_API'] || '';
-}
